c26a96edfb830df903b85f827c24f1bb9df3fd4b
annelieselovespugs <annelieseardizzone@Brynhildr.local>
2020-11-16 13:08:41 -0800

    Added some JavaScript!
    Changed the way that text links to a different page. (i.e. I gave "Favorite Characters", "About Me", and "Fun and Games" IDs and used JavaScript to make them load a different page when clicked)

--- public/main.js
+++ public/main.js
@@ -1,11 +1,24 @@
 //$("#load_chocolate") = document.getElementById("#load_chocolate")
 //First option is JQueary
 
-//Functions
-function $requestAboutMe(){
-  unloadContent();
-  $.ajax({url: "AboutMe.html", success: loadContent})
+document.getElementById("about_me").onclick = function(){
+  location.href = "AboutMe.html";
 }
 
+document.getElementById("favorite_characters").onclick = function(){
+  location.href = "FavoriteCharacters.html";
+}
+
+document.getElementById("fun_and_games").onclick = function(){
+  location.href = "FunAndGames.html";
+}
+
+
+//Functions
+/*function $requestAboutMe(){
+  unloadContent();
+  $.ajax({url: "AboutMe.html", success: loadContent})
+}*/
+
 //Events
-$("#load_about_me").click(requestAboutMe);
+//$("#load_about_me").click(requestAboutMe);
